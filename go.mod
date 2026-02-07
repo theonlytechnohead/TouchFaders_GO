cdc66f52a9a72edf63aba3e18f8cbd20c1519453
Craig Anderson <cranderson0110@gmail.com>
2026-02-07 19:43:26 +1300

    [ Init ] Initial test implementation, acquisition of libraries

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theonlytechnohead/TouchFaders_GO
+
+go 1.25.7
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	var address string
+
+	fmt.Print("Enter console IP address: ")
+	fmt.Scan(&address)
+	fmt.Println("The IP address is:", address)
+
+	var fqdn = address + ":12300"
+
+	conn, err := net.Dial("tcp", fqdn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	status, err := bufio.NewReader(conn).ReadByte()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Read a byte!", status)
+
+	conn.Close()
+}
